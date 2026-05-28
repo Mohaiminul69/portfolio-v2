cd6526ef8926dae719c160c23737fa0e379b6c8c
mohaiminul69 <mohaiminul69@gmail.com>
2026-05-28 16:47:18 +0600

    hero section updated.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
-import heroImg from "../../assets/images/hero-pic.png";
+// import heroImg from "../../assets/images/hero-pic.png";
 import "./index.css";
 
 const Home = () => {
   return (
     <div className="home-page">
       <div className="image-wrapper">
-        <img src={heroImg} alt="Hero" srcset="" />
+        <div className="hero-image">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {/* <img src={heroImg} alt="Hero" srcset="" /> */}
       </div>
     </div>
   );
